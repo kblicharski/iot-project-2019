464f7d50c87e0d7d63bb7ee20a10ec47b46d828c
Kevin Blicharski <blicharskikevin@gmail.com>
2019-04-17 14:33:04 -0500

    chore: websocket boilerplate

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'Test', email: 'test@example.com', password: 'test', password_confirmation: 'test')
