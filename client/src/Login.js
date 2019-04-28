9ed1c1413ab9463681b8402b8915159b507aaa10
madsilva <madeline-silva@uiowa.edu>
2019-04-28 05:07:37 -0500

    Add styling to login page.

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,15 @@
-import React from 'react';
+import React from 'react'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 import AuthService from './AuthService'
+import history from './history'
 
-import history from './history';
-
-function Login() {
-  return (
-    <div>
-      login!!!!!
-      <LoginForm />
-    </div>
-  )
-}
-
-class LoginForm extends React.Component {
+class Login extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -25,52 +21,74 @@ class LoginForm extends React.Component {
     this.Auth = new AuthService()
   }
 
-  componentWillMount(){
-    if(this.Auth.loggedIn())
-      history.replace('/');
-}
+  componentWillMount() {
+    // Redirect to home if already logged in
+    if(this.Auth.loggedIn()) {
+      history.replace('/')
+    }
+  }
 
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
-    });
+    })
   }
 
-  handleSubmit (event) {
-    event.preventDefault();
+  handleSubmit(event) {
+    event.preventDefault()
     this.Auth.login(this.state.username, this.state.password)
       .then(res => {
-        history.replace('/');
+        history.replace('/')
       })
       .catch(err => {
-        alert(err);
+        alert(err)
       })
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        Username:
-        <input
-          type="text"
-          name="username"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-
-        Password:
-        <input
-          type="password"
-          name="password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Submit"/>
-    </form>
+      <Container fluid="true"  className="center">
+        <div>
+        <div className="littlebox">
+        <Row>
+          <Col>
+            <h1>Please log in</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Form onSubmit={this.handleSubmit}>
+              <Form.Group controlId="username">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username..."
+                  value={this.state.username}
+                  onChange={this.handleChange}
+                  name="username"
+                />
+              </Form.Group>
+              <Form.Group controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={this.state.password}
+                  onChange={this.handleChange}
+                  type="password"
+                  placeholder="Enter password..."
+                  name="password"
+                />
+              </Form.Group>
+              <button id="special-button" type="submit">
+                Submit
+              </button>
+            </Form>
+          </Col>
+        </Row>
+        </div>
+        </div>
+      </Container>
     )
   }
 }
-
-
 
 export default Login;
