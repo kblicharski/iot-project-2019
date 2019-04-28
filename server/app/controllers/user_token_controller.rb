8ee1d18396d9361d11221029b13ecd88ee6fc476
Kevin Blicharski <blicharskikevin@gmail.com>
2019-04-28 05:25:21 -0500

    bug: fix issue with knock not signing with our secret key

--- server/app/controllers/user_token_controller.rb
+++ server/app/controllers/user_token_controller.rb
@@ -1,4 +1,3 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
-  config.token_secret_signature_key = -> { Rails.application.credentials.fetch(:secret_key_base) }
 end
