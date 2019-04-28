f968d6facc36f3476778b12a312903bb58c87f9c
Kevin Blicharski <blicharskikevin@gmail.com>
2019-04-28 03:11:07 -0500

    feat: prep for deployment

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
-  post 'debug', to: 'configs#debug', as: :debug
-  resources :configs, only: %i[index create]
+  get 'app/state', to: 'configs#index', as: :index
+  post 'app/settings', to: 'configs#create', as: :create
   resources :temperatures, only: %i[index create]
   resources :humidities, only: %i[index create]
   post 'heat_lamp', to: 'lamps#set_heat_lamp', as: :set_heat_lamp
