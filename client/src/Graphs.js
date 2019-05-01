ad099dcd3b047b37970f94b3ee7f3abd32163b40
Kevin Blicharski <blicharskikevin@gmail.com>
2019-05-01 10:14:51 -0500

    style graph

--- client/src/Graphs.js
+++ client/src/Graphs.js
@@ -21,7 +21,7 @@ class Graphs extends React.Component {
 
     let end = new Date()
     let start = new Date();
-    start.setHours(end.getHours()-1.5)
+    start.setHours(end.getHours()-24)
 
     let url = `${auth.domain}/temperatures/?start=${start.valueOf()}&ending=${end.valueOf()}`
 
@@ -43,17 +43,27 @@ class Graphs extends React.Component {
         this.setState({
           data: responseJson
         })
+        this.setState(
+          { data: this.state.data.map(temp => {
+              return {
+                  value: temp.value,
+                  created_at: ((new Date(temp['created_at'])).toLocaleString('en-US', { hour12: true }))
+              }
+          })
+        });
       })
   }
   
   render() {
+    const hStyle = {padding: "20px"};
     return(
       <Row>
         <Col xs>
+          <h3 style={hStyle}>Temperature</h3>
           <LineChart width={900} height={500} data={this.state.data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
             <Line type="monotone" dataKey="value" stroke="#8884d8" />
             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-            <XAxis dataKey="created-at" minTickGap="25"/>
+            <XAxis dataKey="created_at" minTickGap="5" />
             <YAxis domain={['dataMin-1', 'dataMax+1']} />
             <Tooltip />
           </LineChart>
